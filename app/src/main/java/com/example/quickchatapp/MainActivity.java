087ea288dcc0fae62a97e37d9fc90e972436d872
Bhaveshmn <bhaveshmo573@gmail.com>
2021-05-11 17:28:12 +0530

    splash screen

--- app/src/main/java/com/example/quickchatapp/MainActivity.java
+++ app/src/main/java/com/example/quickchatapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.quickchatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
